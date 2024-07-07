71d7d02e7289017ae2e3a3d996b39044bf01d1ac
chamalanuradha <chamalanuradha98765@gmail.com>
2024-07-07 11:49:35 +0530

    add service files , environment file and constant file

--- src/app/service/station.service.ts
+++ src/app/service/station.service.ts
@@ -1,10 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { CONSTANT } from '../constant/constant';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StationService {
+apiEndPoint : string ='';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) { 
+    this.apiEndPoint = environment.ApiEndPoint;
+  }
+
+  getAllStation(){
+    return this.http.get(this.apiEndPoint + CONSTANT.ENDPOINTS.GET_ALL_STATION)
+  }
 }
