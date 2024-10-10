37521a0f80dd7bfac78db8f8d3a78fc723c0e4fb
chamalanuradha <chamalanuradha98765@gmail.com>
2024-10-10 08:08:51 +0530

    update code

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { IStation, ResponseModel } from '../../models/station';
+import { StationService } from '../../service/station.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+stationList : IStation[] = []
+
+  constructor (private stationsrv:StationService){
+
+  }
+  ngOnInit(): void{
+    this.loadStation();
+  }
+
+  loadStation(){
+    this.stationsrv.getAllStation().subscribe((res:ResponseModel) =>{
+      this.stationList = res.data;
+
+    },error => {
+      alert("error" + JSON.stringify(error))
+    })
+  }
 
 }
--- src/app/service/station.service.ts
+++ src/app/service/station.service.ts
@@ -2,18 +2,27 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { CONSTANT } from '../constant/constant';
+import { Observable, throwError } from 'rxjs';
+import { ResponseModel } from '../models/station';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StationService {
-apiEndPoint : string ='';
+  apiEndPoint: string = '';
 
-  constructor(private http:HttpClient) { 
+  constructor(private http: HttpClient) { 
     this.apiEndPoint = environment.ApiEndPoint;
   }
 
-  getAllStation(){
-    return this.http.get(this.apiEndPoint + CONSTANT.ENDPOINTS.GET_ALL_STATION)
+  getAllStation(): Observable<ResponseModel> {
+    return this.http.get<ResponseModel>(this.apiEndPoint + CONSTANT.ENDPOINTS.GET_ALL_STATION)
+      .pipe(
+        catchError((error) => {
+          console.error('Error fetching stations:', error);
+          return throwError(() => new Error('Error fetching stations'));
+        })
+      );
   }
 }
