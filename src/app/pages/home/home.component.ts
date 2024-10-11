07d7d2f377f6266c64e17773255179641471cfcb
chamalanuradha <chamalanuradha98765@gmail.com>
2024-10-11 19:57:24 +0530

    update code

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,22 +2,28 @@ import { Component, OnInit} from '@angular/core';
 import { IStation } from '../../models/station';
 import { StationService } from '../../service/station.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  styleUrls: ['./home.component.css'],
 
 })
 export class HomeComponent implements OnInit {
 
 stationList : IStation[] = []
 
+fromStationId: number = 0;
+toStationId: number = 0;
+dateOfTravel: string = '';
+
   constructor (private stationsrv:StationService){
   }
+  
   ngOnInit(): void{
     this.loadStation();
   }
@@ -25,7 +31,16 @@ stationList : IStation[] = []
   loadStation(){
     this.stationsrv.getAllStation().subscribe((res:any) =>{
       this.stationList = res.data;
-      console.log(this.stationList);
     }) 
   }
+
+  onSearch(){
+    if (this.fromStationId == 0 || this.toStationId == 0 || this.dateOfTravel == ' '){
+      alert("Please select station")
+    }
+    else if (this.fromStationId == this.toStationId){
+      alert("Please select different station")
+    }
+  }
+
 }
--- src/app/service/station.service.ts
+++ src/app/service/station.service.ts
@@ -8,11 +8,8 @@ import { Injectable } from '@angular/core';
 
 export class StationService {
   apiEndPoint: string = 'https://freeapi.miniprojectideas.com/api/TrainApp';
-
   constructor(private http: HttpClient) { 
-
   }
-
   getAllStation () {
     return this.http.get(`${this.apiEndPoint}/GetAllStations`)
   }
