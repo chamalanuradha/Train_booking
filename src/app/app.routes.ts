ae4d47b9a849eba771c2a432c47cb2ff5d6ec5aa
chamalanuradha <chamalanuradha98765@gmail.com>
2024-10-14 07:34:48 +0530

    update code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
-import { BookingComponent } from './pages/booking/booking.component';
-import { AdminComponent } from './pages/admin/admin.component';
+
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    { path: 'search', component: SearchComponent },
-    { path: 'booking', component: BookingComponent },
-    { path: 'admin' , component: AdminComponent}
+    {
+         path: '',
+         redirectTo: 'home', 
+         pathMatch: 'full' },
+
+    { 
+        path: 'home', 
+        component: HomeComponent },
+
+    { 
+        path: 'search/:fromStationId/:toStationId/:dateOfTravel', 
+        component: SearchComponent },
+  
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { IStation } from '../../models/station';
 import { StationService } from '../../service/station.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -21,9 +22,9 @@ fromStationId: number = 0;
 toStationId: number = 0;
 dateOfTravel: string = '';
 
-  constructor (private stationsrv:StationService){
+  constructor (private stationsrv:StationService, private router: Router){
   }
-  
+
   ngOnInit(): void{
     this.loadStation();
   }
@@ -36,10 +37,13 @@ dateOfTravel: string = '';
 
   onSearch(){
     if (this.fromStationId == 0 || this.toStationId == 0 || this.dateOfTravel == ' '){
-      alert("Please select station")
+      alert("Please select relevant data")
     }
     else if (this.fromStationId == this.toStationId){
       alert("Please select different station")
+    }
+    else {
+      this.router.navigate(['/search',this.fromStationId, this.toStationId,this.dateOfTravel])
     }
   }
 
